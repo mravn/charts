992e11e9cdec5a9fb626d6e4c7b62c0d6c558a9d
Mikkel Nygaard Ravn <mikkel.nygaard.ravn@gmail.com>
2018-10-17 19:31:26 +0200

    Add Bar and BarTween

--- lib/main.dart
+++ lib/main.dart
@@ -3,6 +3,8 @@ import 'dart:math';
 import 'package:flutter/animation.dart';
 import 'package:flutter/material.dart';
 
+import 'bar.dart';
+
 void main() {
   runApp(MaterialApp(home: ChartPage()));
 }
@@ -14,9 +16,8 @@ class ChartPage extends StatefulWidget {
 
 class ChartPageState extends State<ChartPage> with TickerProviderStateMixin {
   final random = Random();
-  int dataSet = 50;
   AnimationController animation;
-  Tween<double> tween;
+  BarTween tween;
 
   @override
   void initState() {
@@ -25,7 +26,7 @@ class ChartPageState extends State<ChartPage> with TickerProviderStateMixin {
       duration: const Duration(milliseconds: 300),
       vsync: this,
     );
-    tween = Tween<double>(begin: 0.0, end: dataSet.toDouble());
+    tween = BarTween(Bar(0.0), Bar(50.0));
     animation.forward();
   }
 
@@ -37,10 +38,9 @@ class ChartPageState extends State<ChartPage> with TickerProviderStateMixin {
 
   void changeData() {
     setState(() {
-      dataSet = random.nextInt(100);
-      tween = Tween<double>(
-        begin: tween.evaluate(animation),
-        end: dataSet.toDouble(),
+      tween = BarTween(
+        tween.evaluate(animation),
+        Bar(random.nextDouble() * 100.0),
       );
       animation.forward(from: 0.0);
     });
@@ -61,34 +61,4 @@ class ChartPageState extends State<ChartPage> with TickerProviderStateMixin {
       ),
     );
   }
-}
-
-class BarChartPainter extends CustomPainter {
-  static const barWidth = 10.0;
-
-  BarChartPainter(Animation<double> animation)
-      : animation = animation,
-        super(repaint: animation);
-
-  final Animation<double> animation;
-
-  @override
-  void paint(Canvas canvas, Size size) {
-    final barHeight = animation.value;
-    final paint = Paint()
-      ..color = Colors.blue[400]
-      ..style = PaintingStyle.fill;
-    canvas.drawRect(
-      Rect.fromLTWH(
-        (size.width - barWidth) / 2.0,
-        size.height - barHeight,
-        barWidth,
-        barHeight,
-      ),
-      paint,
-    );
-  }
-
-  @override
-  bool shouldRepaint(BarChartPainter old) => false;
 }
